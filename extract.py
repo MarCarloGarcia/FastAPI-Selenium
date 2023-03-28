1e2b0b8126b7ad0a2c5c6f6996ace44c746cdbc8
MCG <128881622+MarCarloGarcia@users.noreply.github.com>
2023-03-28 12:08:43 +0800

    use sauce labs(webdriver.Remote)

--- extract.py
+++ extract.py
@@ -1,7 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
+USERNAME = 'oauth-mcarlo.rg-11d9a'
+ACCESS_KEY = 'b19834d6-f40b-4b6b-a347-3f8d2a5b11ba'
+
+
+def createDriver() -> webdriver.Chrome:
+    # Set options
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless') # example option
+
+    # Set desired capabilities
+    caps = webdriver.DesiredCapabilities.CHROME.copy()
+    caps['version'] = 'latest'
+    caps['platform'] = 'Windows 10'
+    caps['username'] = USERNAME
+    caps['accessKey'] = ACCESS_KEY
+
+    # Create remote webdriver object
+    myDriver = webdriver.Remote(
+        command_executor='https://oauth-mcarlo.rg-11d9a:b19834d6-f40b-4b6b-a347-3f8d2a5b11ba@ondemand.eu-central-1.saucelabs.com:443/wd/hub',
+        options=options,
+        desired_capabilities=caps
+    )
+    return myDriver
+
+"""
 def createDriver() -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
@@ -18,7 +44,7 @@ def createDriver() -> webdriver.Chrome:
         options=chrome_options
     )
     return myDriver
-
+"""
 def getGoogleHomepage(driver: webdriver.Chrome) -> str:
     driver.get("https://www.google.com")
     return driver.page_source
